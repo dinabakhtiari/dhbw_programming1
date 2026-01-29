cd57ee5eadd64d5a80a1cd7bd3da5cd7b555cd61
Dina Bakhtiari <dinaba701@gmail.com>
2026-01-29 22:49:56 +0100

    code with the first mathematical operator : +

---
+++ Basics_java/Mathematical_Operators.java
@@ -0,0 +1,7 @@
+public class Mathematical_Operators {
+    public static void main(String[] args) {
+        // + - * / %
+        int result = 10 + 6;
+        System.out.println("Result: " + result);
+    }
+}
