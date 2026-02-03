dcd9ed842eb3ae28e27c29a9c2229e840b09ff11
Dina Bakhtiari <dinaba701@gmail.com>
2026-02-03 23:10:07 +0100

    implement string to integer casting

---
+++ Basics_java/TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting {
+    public static void main(String[] args){
+        String num1 = "20";
+        int num2 = Integer.parseInt(num1) + 3;
+        System.out.println(num2);
+        
+    }
+}
