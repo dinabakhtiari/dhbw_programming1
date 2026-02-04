addf511ee1afd5f94638923c11cd1db36d5d36e1
Dina Bakhtiari <dinaba701@gmail.com>
2026-02-04 09:19:29 +0100

    working with data types

---
+++ DataType/Variables.java
@@ -0,0 +1,14 @@
+package DataType;
+
+public class Variables {
+    public static void main(String[] args) {
+            String name = "Dina Bakhtiari";
+            System.out.println(name);
+
+            int myNum = 15;
+            System.out.println(myNum);
+
+            float myFloatNum = 5.99f;
+            System.out.println(myFloatNum);
+    }
+}
