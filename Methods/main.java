ee0140d5b9a1f80c5085e5c59da68275ed744bf9
Dina Bakhtiari <dinaba701@gmail.com>
2026-02-18 09:09:31 +0100

    a method to add all numbers from 5 to 10 using recursion.

--- Methods/main.java
+++ Methods/main.java
@@ -1,15 +1,22 @@
 package Methods;
 public class main {
-  public static int sum(int k) {
-    if (k > 0) {
-      return k + sum(k - 1);
+//  public static int sum(int k) {
+   // if (k > 0) {
+    //  return k + sum(k - 1);
+    //} else {
+    //  return 0;
+   // }
+  //}
+public static int sum(int start, int end) {
+    if (end > start) {
+      return end + sum(start, end - 1);
     } else {
-      return 0;
+      return end;
     }
   }
-
   public static void main(String[] args) {
-    int result = sum(10);
+   int result = sum(5, 10);
     System.out.println(result);
   }
 }
+
